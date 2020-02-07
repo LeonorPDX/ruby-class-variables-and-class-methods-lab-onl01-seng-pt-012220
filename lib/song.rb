6f1f9ec0fdef0485149c903d1a545336f27ae7f3
Leonor Colbert <leonoracolbert@gmail.com>
2020-02-07 20:23:19 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,16 @@
 class Song
   @@songs = 0
+  @@artists = []
+  @@genres = []
+  
+  def self.genre_count
+    @@genres.length
+  end
   
   def initialize(name, artist, genre)
-    @@songs +=1
+    @@songs += 1
+    @@artists << artist
+    @@genre << genre
   end
   
 end
